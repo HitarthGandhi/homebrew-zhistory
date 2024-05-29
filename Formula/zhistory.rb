cd7ab86091914870618cdaedf6dea48b247bc87d
HitarthGandhi <hitarthgandhi01@gmail.com>
2024-05-29 12:17:08 -0700

    added version

--- Formula/zhistory.rb
+++ Formula/zhistory.rb
@@ -4,6 +4,7 @@ class Zhistory < Formula
   url "https://github.com/itsKarad/ZHistory/blob/hitarth/releases/zhistory.tar.gz"
   sha256 "b2e9792d38d101ea8c2ed28e8fc6f73b355c5d3fd8c814c245a13bbef9b4dbf4"
   license "MIT"
+  version "0.1.0"
 
   depends_on "rust" => :build
 
